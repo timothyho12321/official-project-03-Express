da39c026d3981f69650c9386e098ca75a8d5d93e
timothyho12321 <timhotim2@gmail.com>
2022-12-23 10:48:46 +0000

    db migrate created two tables smells and purposes

---
+++ migrations/20221223103527-add-default-purposes.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function (options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function (db) {
+  db.insert("purposes", ['purpose'], ['Moisturise']);
+  db.insert("purposes", ['purpose'], ['Anti-bacteria']);
+  db.insert("purposes", ['purpose'], ['Sensitive']);
+  db.insert("purposes", ['purpose'], ['Oil-removing']);
+
+  return null;
+};
+
+exports.down = function (db) {
+  return null;
+};
+
+exports._meta = {
+  "version": 1
+};
--- routes/landing.js
+++ routes/landing.js
@@ -5,12 +5,21 @@ const router = express.Router();
 
 router.get('/', (req,res)=>{
 
-
-
-
     res.render('landing/index')
 })
 
+
+router.get('/about-us', (req,res)=>{
+
+
+    res.render('landing/about-us')
+})
+
+router.get('/contact-us', (req,res)=>{
+
+
+    res.render('landing/contact-us')
+})
 
 
 module.exports = router;
